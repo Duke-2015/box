e6db2af7aadaf0aa1a11f9252f8c2bc2d1e9802e
EvgenNeiman <evg_sqrt@mail.ru>
2015-07-25 22:52:06 +0300

    First commit from Evg

--- Lesson_5/src/Main.java
+++ Lesson_5/src/Main.java
@@ -1,6 +1,8 @@
+//package om.java.lesson.task01;
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello Main method from Dimasss!!");
+		System.out.println("You are stupid boys!");
 	}
 }
